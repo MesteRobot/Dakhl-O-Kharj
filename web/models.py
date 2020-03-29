aff8a29c2bf6ada138db17a492f1f7b8befa5458
Mr.Robot <hassan.doodiyan@gmail.com>
2020-03-29 23:27:25 +0430

    Create a model *Expence* and add one row to it

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Expence(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now=True)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
