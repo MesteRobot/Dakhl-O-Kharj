20b9cf5ae421b60d8f5c2855819a722e6582d4ce
Mr.Robot <hassan.doodiyan@gmail.com>
2020-03-30 23:48:37 +0430

    Add some beautifications to models

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from web.models import *
+from web.models import Expence, Income
 
 
 # Register your models here.
 admin.site.register(Expence)
+admin.site.register(Income)
+
--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,4 +8,19 @@ class Expence(models.Model):
     date = models.DateTimeField(auto_now=True)
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} -- {}".format(self.date, self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now=True)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} -- {}".format(self.date, self.amount)
+
+
+
 
